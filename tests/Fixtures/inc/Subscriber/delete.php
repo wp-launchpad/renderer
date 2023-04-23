3bf10ad17f38a15bb19238f1c11021215e4a71f6
COQUARD Cyrille <coquardcyr@gmail.com>
2023-04-23 07:43:58 +0200

    Added test for delete method

---
+++ tests/Fixtures/inc/Subscriber/delete.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'shouldDelete' => [
+        'config' => [
+              'template' => 'view',
+              'configurations' => [],
+              'json' => 'json',
+              'hash' => 'hash',
+        ],
+        'expected' => [
+            'parameters' => [
+
+            ],
+            'json' => 'json',
+            'key' => 'view-hash'
+        ]
+    ],
+
+];
---
+++ tests/Unit/inc/Subscriber/delete.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace LaunchpadRenderer\Tests\Unit\inc\Subscriber;
+
+use Mockery;
+use LaunchpadRenderer\Subscriber;
+use Psr\SimpleCache\CacheInterface;
+use LaunchpadRenderer\Renderer\Renderer;
+
+
+use LaunchpadRenderer\Tests\Unit\TestCase;
+use Brain\Monkey\Functions;
+
+/**
+ * @covers \LaunchpadRenderer\Subscriber::delete
+ */
+class Test_delete extends TestCase {
+
+    /**
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * @var bool
+     */
+    protected $renderer_cache_enabled;
+
+    /**
+     * @var CacheInterface
+     */
+    protected $cache;
+
+    /**
+     * @var Renderer
+     */
+    protected $renderer;
+
+    /**
+     * @var Subscriber
+     */
+    protected $subscriber;
+
+    public function set_up() {
+        parent::set_up();
+        $this->prefix = '';
+        $this->renderer_cache_enabled = false;
+        $this->cache = Mockery::mock(CacheInterface::class);
+        $this->renderer = Mockery::mock(Renderer::class);
+
+        $this->subscriber = new Subscriber($this->prefix, $this->renderer_cache_enabled, $this->cache, $this->renderer);
+    }
+
+    /**
+     * @dataProvider configTestData
+     */
+    public function testShouldDoAsExpected( $config, $expected )
+    {
+        Functions\expect('wp_encode_json')->with($expected['parameters'])->andReturn($config['json']);
+        Functions\expect('wp_hash')->with($expected['json'])->andReturn($config['hash']);
+        $this->cache->expects()->delete($expected['key']);
+        $this->subscriber->delete($config['template'], $config['configurations']);
+    }
+}
